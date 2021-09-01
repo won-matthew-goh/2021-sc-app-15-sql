714fbe0aaa1ccddd837aa5cb5d4417fb5239f498
won-matthew-goh <qimo2000@naver.com>
2021-09-01 12:41:08 +0900

    first

---
+++ 01.app.js
@@ -0,0 +1,43 @@
+// npm i express lodash numeral moment dotenv mysql2 sequelize passport uuid multer http-errors express-session helmet morgan
+
+/*************** global init ****************/
+require('dotenv').config()
+const port = process.env.PORT
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+/*************** view engine ***************/
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.locals.pretty = true
+
+
+/*************** middleware ****************/
+// application/json
+app.use(express.json())
+
+// application/x-www-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+
+/*************** static init ****************/
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+
+/*************** router init ****************/
+// const Router = require('./routes/')
+
+// app.use('/', Router)
+
+
+/*************** error init *****************/
+const notFoundRouter = require('./routes/error/404')
+const errorRouter = require('./routes/error/500')
+app.use(notFoundRouter)
+app.use(errorRouter)
+
+
+/*************** server init ****************/
+app.listen(port, () => { console.log('http://127.0.0.1:'+port) })
