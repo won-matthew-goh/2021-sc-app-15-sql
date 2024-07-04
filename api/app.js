b4d8e81fe2615f49358947510675f3b3db16c9a4
won-matthew-goh <qimo2000@naver.com>
2024-07-04 23:40:32 +0900

    vue book 최신 문법과 비교 후 주석 저장

--- api/app.js
+++ api/app.js
@@ -1,40 +1,93 @@
 /************* global require *************/
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
-const helmet = require('helmet')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 /*************** server init **************/
-require('./modules/server-init')(app, process.env.PORT)
+require('./modules/server-init')(app, process.env.PORT);
 
 /*************** static init **************/
-app.use(helmet({ contentSecurityPolicy: false }))
+app.use(helmet({ contentSecurityPolicy: false }));
 
 /*************** middleware ***************/
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors({ origin: true, credentials: true }))
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
 
 /*************** logger init **************/
 // app.use(logger)
 
-
 /*************** router init **************/
-const bookRouter = require('./routes/book')
+const bookRouter = require('./routes/book');
 
-app.use('/book', bookRouter)
-
+app.use('/book', bookRouter);
 
 /**************** error init **************/
-const _404Router = require('./routes/error/404-router')
-const _500Router = require('./routes/error/500-router')
+const _404Router = require('./routes/error/404-router');
+const _500Router = require('./routes/error/500-router');
 
-app.use(_404Router)
-app.use(_500Router)
+app.use(_404Router);
+app.use(_500Router);
 
+// /************* global require *************/
+// // 환경 변수 설정을 위한 dotenv 모듈 로드
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const jwt = require('jsonwebtoken');
+// const helmet = require('helmet');
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+
+// /*************** server init **************/
+// // 서버 초기화 및 포트 설정
+// require('./modules/server-init')(app, process.env.PORT);
+
+// /*************** static init **************/
+// // 보안 강화를 위한 helmet 모듈 설정 (특정 내용 보안 정책 비활성화)
+// app.use(helmet({ contentSecurityPolicy: false }));
+
+// /*************** middleware ***************/
+// // JSON 및 URL 인코딩된 데이터 파싱
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// // CORS 설정
+// const allowedOrigins = ['http://localhost:8081']; // 클라이언트 도메인 설정
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // 요청 도메인이 허용된 도메인인지 확인
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true, // 쿠키 사용을 허용
+// };
+// app.use(cors(corsOptions)); // CORS 미들웨어 사용
+
+// // 쿠키 파싱 미들웨어 사용
+// app.use(cookieParser());
+
+// /*************** logger init **************/
+// // 로깅 미들웨어 (현재 비활성화 상태)
+// // app.use(logger)
+
+// /*************** router init **************/
+// // 책 관련 라우터 설정
+// const bookRouter = require('./routes/book');
+// app.use('/book', bookRouter);
+
+// /**************** error init **************/
+// // 404 에러 처리 라우터
+// const _404Router = require('./routes/error/404-router');
+// // 500 에러 처리 라우터
+// const _500Router = require('./routes/error/500-router');
+// app.use(_404Router);
+// app.use(_500Router);
--- api/middlewares/jwt-mw.js
+++ api/middlewares/jwt-mw.js
@@ -14,55 +14,102 @@
  *      = 리턴된 내용에서 전달 받은 origin과 apikey의 일치 여부 확인
  *        +  일치
  *          . next()
- *        + !일치 
+ *        + !일치
  *          . next(createError(401, 'Authorization Fail'))
- * 
-*/
+ *
+ */
 
-const jwt = require('jsonwebtoken')
-const createError = require('http-errors')
-const { pool } = require('../modules/mysql-init')
-const { findApiUser } = require('../models/auth')
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+const { pool } = require('../modules/mysql-init');
+const { findApiUser } = require('../models/auth');
 
 const createCookie = (domain, apikey, res) => {
-  const token = jwt.sign({ domain, apikey }, process.env.JWT_SALT, { expiresIn: Number(process.env.JWT_EXPIRES) })
-  res.cookie('token', token, { expires: new Date(Date.now() + Number(process.env.JWT_EXPIRES)) })
-}
+  const token = jwt.sign({ domain, apikey }, process.env.JWT_SALT, { expiresIn: Number(process.env.JWT_EXPIRES) });
+  res.cookie('token', token, { expires: new Date(Date.now() + Number(process.env.JWT_EXPIRES)) });
+};
 
 const isApiUser = async (req, res, next) => {
-  const errMsg = 'Authorization Fail'
+  const errMsg = 'Authorization Fail';
   try {
-    console.log(req.headers)
-    const domain = req.headers.origin || req.protocol + "://" + req.headers.host
-    const apikey = req.query.apikey
+    console.log(req.headers);
+    const domain = req.headers.origin || req.protocol + '://' + req.headers.host;
+    const apikey = req.query.apikey;
 
     if (req.cookies.token) {
-      const token = jwt.verify(req.cookies.token, process.env.JWT_SALT)
+      const token = jwt.verify(req.cookies.token, process.env.JWT_SALT);
       if (domain === token.domain && apikey === token.apikey) {
-        createCookie(domain, apikey, res)
-        next()
+        createCookie(domain, apikey, res);
+        next();
+      } else {
+        next(createError(401, errMsg));
       }
-      else {
-        next(createError(401, errMsg))
-      }
-    }
-    else if (domain && apikey) {
-      const { success } = await findApiUser(domain, apikey)
+    } else if (domain && apikey) {
+      const { success } = await findApiUser(domain, apikey);
       if (success) {
-        createCookie(domain, apikey, res)
-        next()
+        createCookie(domain, apikey, res);
+        next();
+      } else {
+        next(createError(401, errMsg));
       }
-      else {
-        next(createError(401, errMsg))
-      }
+    } else {
+      next(createError(401, errMsg));
     }
-    else {
-      next(createError(401, errMsg))
-    }
+  } catch (err) {
+    next(createError(err));
   }
-  catch (err) {
-    next(createError(err))
-  }
-}
+};
 
-module.exports = { isApiUser }
+module.exports = { isApiUser };
+
+// const jwt = require('jsonwebtoken');
+// const createError = require('http-errors');
+// const { pool } = require('../modules/mysql-init');
+// const { findApiUser } = require('../models/auth');
+
+// // 쿠키 생성 함수
+// const createCookie = (domain, apikey, res) => {
+//   // JWT 토큰 생성
+//   const token = jwt.sign({ domain, apikey }, process.env.JWT_SALT, { expiresIn: Number(process.env.JWT_EXPIRES) });
+//   // 쿠키에 토큰 저장
+//   res.cookie('token', token, { expires: new Date(Date.now() + Number(process.env.JWT_EXPIRES)) });
+// };
+
+// // API 유저 검증 미들웨어
+// const isApiUser = async (req, res, next) => {
+//   const errMsg = 'Authorization Fail';
+//   try {
+//     console.log(req.headers);
+//     // 요청 도메인 설정
+//     const domain = req.headers.origin || req.protocol + '://' + req.headers.host;
+//     // 헤더에서 apikey 가져오기
+//     const apikey = req.headers['x-api-key'];
+
+//     if (req.cookies.token) {
+//       // 쿠키에서 토큰 검증
+//       const token = jwt.verify(req.cookies.token, process.env.JWT_SALT);
+//       // 도메인과 apikey 검증
+//       if (domain === token.domain && apikey === token.apikey) {
+//         createCookie(domain, apikey, res); // 쿠키 갱신
+//         next(); // 다음 미들웨어로 이동
+//       } else {
+//         next(createError(401, errMsg)); // 인증 실패
+//       }
+//     } else if (domain && apikey) {
+//       // DB에서 유저 정보 확인
+//       const { success } = await findApiUser(domain, apikey);
+//       if (success) {
+//         createCookie(domain, apikey, res); // 쿠키 생성
+//         next(); // 다음 미들웨어로 이동
+//       } else {
+//         next(createError(401, errMsg)); // 인증 실패
+//       }
+//     } else {
+//       next(createError(401, errMsg)); // 인증 실패
+//     }
+//   } catch (err) {
+//     next(createError(err)); // 에러 처리
+//   }
+// };
+
+// module.exports = { isApiUser };
